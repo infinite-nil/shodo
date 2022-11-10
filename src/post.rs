0abc404dbfc5a91712abb651ad1966def71a2d9a
João Alberto <hey@joaoalberto.dev>
2022-11-10 13:14:44 -0300

    chore: generate mardown files instead html

--- src/post.rs
+++ src/post.rs
@@ -4,7 +4,7 @@ use std::{fs::File, io::Write};
 pub fn generate_post(name: String) -> std::io::Result<()> {
     let slug = slugify(&name);
     let path = "./content/".to_string();
-    let ext = &".html".to_string();
+    let ext = &".md".to_string();
     let file_name = path + &slug + ext;
     let mut file = File::create(file_name)?;
 
